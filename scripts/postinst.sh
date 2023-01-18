af29d530a14315529b017c82fbe804802241b03b
UmaShankar Avagadda <umagmrit@gmail.com>
2023-01-18 11:23:52 +0530

    ZBUG-2722 : Deploy Zimbra-Zimlet-Signature-Template locally (#5)
    * ZBUG-2722 : Deploy zimlet locally
    
    * ZBUG-2722: Updated version

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -6,11 +6,11 @@ if [ $? -ne 0 ]; then
    echo "*** Mailbox is not running... ***"
    echo "*** Follow the steps below as zimbra user ignore if installing through install.sh .. ***"
    echo "*** Install the Zimbra-Zimlet-Signature-Template  zimlet. ***"
-   echo "*** zmzimletctl deploy /opt/zimbra/zimlets-network/zimbra-zimlet-signature-template.zip ***"
+   echo "*** zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-signature-template.zip ***"
    echo "*** zmprov fc zimlet ***"
 else
    echo "*** Deploying Zimbra-Zimlet-Signature-Template  zimlet ***"
-   su - zimbra -c  "zmzimletctl deploy /opt/zimbra/zimlets-network/zimbra-zimlet-signature-template.zip"
+   su - zimbra -c  "zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-signature-template.zip"
    su - zimbra -c  "zmprov fc zimlet"
 fi
 echo "*** Zimbra-Zimlet-Signature-Template Installation Completed. ***"
